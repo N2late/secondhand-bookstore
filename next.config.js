780e75bc7c50559044582fe74dfd939b3d0cda02
Tiago Serra <35219940+N2late@users.noreply.github.com>
2022-10-26 12:31:35 +0200

    Implement PWA ftr

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   compiler: {
     emotion: true,
   },
   swcMinify: true,
-};
+});
 
 module.exports = nextConfig;
